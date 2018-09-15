6214ab0edd4ccf45d973da32ea4c581b7d55d1d6
Thomas Phillips <thomasphillips3@gmail.com>
2018-09-14 20:02:25 -0700

    Test to make sure contract creator is assigned manager role

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -36,4 +36,9 @@ describe('Campaigns', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
     });
+
+    it('should assign manager role to contract creator', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
 });
